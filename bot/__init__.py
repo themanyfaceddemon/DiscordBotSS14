bf6a4098dd1be0db127900f8b7f0730013ca7a65
themanyfaceddemon <neko-d-05@mail.ru>
2024-05-06 04:00:21 +0300

    update __init__ of main bot

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,12 +2,13 @@ import discord
 import toml
 from discord.ext import commands
 
+from classes.db_manager import DBManager
 from classes.lang import LanguageManager
 
 with open("config.toml", "r") as file:
     config = toml.load(file)
-    command_prefix = config["bot"]["command_prefix"]
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=command_prefix, intents=intents, help_command=None)
-lang = LanguageManager()
+bot = commands.Bot(command_prefix=config["bot"]["prefix"], intents=intents, help_command=None)
+lang = LanguageManager(config["lang"]["code"])
+db_manager = DBManager(config["db"]["name"])
