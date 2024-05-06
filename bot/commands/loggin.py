b566db98f01ac8c880d5c7834899477e99819fb2
themanyfaceddemon <neko-d-05@mail.ru>
2024-05-06 04:00:40 +0300

    add db_manager into loggin command

--- bot/commands/loggin.py
+++ bot/commands/loggin.py
@@ -2,15 +2,12 @@ import discord
 import discord.ext
 import discord.ext.commands
 from discord import Option
-import toml
 
-from bot import bot, lang
+from bot import bot, config, db_manager, lang
 from classes.ss14login import SS14Login
-from classes.lang import LanguageManager
 
 
-
-@bot.slash_command(name="loggin", description=lang.loc('loggin', 'com_desc'))
+@bot.slash_command(name="loggin", description=lang.loc('loggin', 'com_desc'), guild_ids=list[discord.Guild](config["bot"]["servers"]))
 async def loggin(
     ctx:           discord.ApplicationContext,
     login:         Option(str, description=lang.loc('loggin', 'arg_desc_login'), required=True),   # type: ignore
@@ -20,6 +17,7 @@ async def loggin(
 
     if ss14login.login(login, password):
         resp = lang.loc('loggin', 'correct')
+        db_manager.add_or_update_member(ctx.author.id, login, None) # There may be a problem if a person enters email
     else:
         resp = lang.loc('loggin', 'error')
 
