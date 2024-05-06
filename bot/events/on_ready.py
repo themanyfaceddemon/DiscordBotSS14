87813f4a46cc10d0ca39bb2598b75a4dae896e83
themanyfaceddemon <neko-d-05@mail.ru>
2024-05-06 04:01:00 +0300

    add read_role start in on_ready

--- bot/events/on_ready.py
+++ bot/events/on_ready.py
@@ -4,8 +4,12 @@ import discord
 import discord.ext
 
 from bot import bot
+from bot.events.read_role import read_role
 
 
 @bot.event
 async def on_ready():
+    logging.info(f"Start read_role loop")
+    read_role.start()
+
     logging.info(f"{bot.user} is ready and online!")
